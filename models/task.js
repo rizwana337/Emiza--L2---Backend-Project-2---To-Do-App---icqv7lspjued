08b710978c373548a17e7ff78ce85940eea70373
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-05-13 20:30:02 +0530

    Update task.js

--- models/task.js
+++ models/task.js
@@ -1,22 +1,52 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-/*
-Complete your other field here
-
-1. "heading" should be a string and required
-2. "description" should be a string and required
-3. "status" should be a string and it can have only 2 values ('pending', 'done') and default should be pending
-4. "creator_id" will be reference to user who have created this task (store the user id of creator).
-5. Also Enable timestamps which will store the date of document created and updated.
-
-*/
-
-
-var taskSchema = mongoose.Schema({
-  //Write your code here.
-},
-{ /* ... */ }
+const taskSchema = new Schema(
+  {
+    heading: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "done"],
+      default: "pending",
+    },
+    creator_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
+module.exports = mongoose.model("Tasks", taskSchema);
 
-module.exports = mongoose.model("Task", taskSchema);;
+
+// var mongoose = require("mongoose");
+
+// /*
+// Complete your other field here
+
+// 1. "heading" should be a string and required
+// 2. "description" should be a string and required
+// 3. "status" should be a string and it can have only 2 values ('pending', 'done') and default should be pending
+// 4. "creator_id" will be reference to user who have created this task (store the user id of creator).
+// 5. Also Enable timestamps which will store the date of document created and updated.
+
+// */
+
+
+// var taskSchema = mongoose.Schema({
+//   //Write your code here.
+// },
+// { /* ... */ }
+// );
+
+
+// module.exports = mongoose.model("Task", taskSchema);;
